0374fc14184e2239cedbe52b96f0ee674a79fefe
Shriram <shreeram51091@gmail.com>
2017-03-10 21:21:25 +0530

    S3 added for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,9 +8,8 @@ Rails.application.configure do
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
       
-      :path =>'/:class/:attachment/:id_partition/:style/:filename',
-      :s3_host_name => 's3-us-west-2.amazonaws.com'
     }
   }
   # Code is not reloaded between requests.
