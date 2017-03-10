cafc062ad42912679f163b363f8a31e1b80d2fb6
Shriram <shreeram51091@gmail.com>
2017-03-11 00:59:02 +0530

    AWS commit for paperclip

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,9 @@ class Book < ActiveRecord::Base
 	has_many :reviews
 
 
+
+
 	has_attached_file :image, styles: { medium: "400x600#", thumb: "200x200#" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, url: ':s3_domain_url',
+  path: '/:class/:attachment/:id_partition/:style/:filename'
 end
